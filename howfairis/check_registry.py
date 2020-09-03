74b93186d14e150840cf7733e752eab96ede7796
Edidiong Etuk <edidiongmichael08@gmail.com>
2020-09-03 16:35:32 +0100

    check_registry code

--- howfairis/check_registry.py
+++ howfairis/check_registry.py
@@ -1,17 +1,20 @@
-import os
-
-def has_pypi_badge(readme_file):
-    with open(readme_file) as text:
-        for line in text:
-            if 'PyPi' in line or 'pypi' in line:
-                return "PyPi is found"
+def has_programming_badge(readme_file):
+    with open(readme_file, "r") as text:
+        text = text.read()
+        for lang, badge in registries["by_language"].items():
+            if badge in text:
+                return lang + " badge found"
 
     return "No registry found"
 
 
 def main():
-    print(has_pypi_badge("README.md"))
+    print(has_programming_badge("README.md"))
 
 
 if __name__ == "__main__":
+    import yaml
+
+    with open("registries.yaml") as opened_file:
+        registries = yaml.safe_load(opened_file)
     main()
